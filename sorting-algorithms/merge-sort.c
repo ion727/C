da168b5c328aa6367f76eec6d3e346adf0fcbd80
ion <151961936+ion727@users.noreply.github.com>
2024-11-22 17:59:07 +0100

    Update merge-sort.c
    Implemented visual representation of the algorithm & CPU-usage runtime.

--- sorting-algorithms/merge-sort.c
+++ sorting-algorithms/merge-sort.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <limits.h>
 
-int * sort(int len, int *left, int *array, int *sorted);
+int * sort(int len, int *left, int *array, int arraylen, int *sorted);
 
 int main(int argc, char *argv[]){
     if (argc < 4) {
-        printf("%s: expected 3 or more arguments, got %i.\n", argv[0], argc-1);
+        printf("Usage: <%s> <int> <int> ... <int>.\nOutputs to stdio a sorted list using the provided integers.\n", argv[0]);
         return 1;
     }
-    
+    clock_t start = clock();
     // stores the length of the array of integers
     int len = argc - 1; 
-
 
     // convert each element of argv into an integer and add it to "args"
     int *args = malloc(len*sizeof(int));
@@ -24,22 +26,27 @@ int main(int argc, char *argv[]){
     }
     for (int i = 0; i < len; i++) {
         args[i] = atoi(argv[i + 1]);
+        sorted[i] = INT_MIN;
     }
 
-    args = sort(len, args, args, sorted);
+    sort(len, args, args, len, sorted);
     
-    for (int i = 0; i < len; i++) {
-        printf("%i ", args[i]);
-    }
-    printf("\n");
-
+    printf("\r[ ");
+    for (int i = 0; i < len; i++) {printf("%i ", args[i]);}
+    printf("]\n");
+    for (int i = 0; i < len; i++) {printf("  ");}
+    printf("   \r");
+    
     free(args);
     free(sorted);
+    clock_t end = clock();
+    double runtime = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Runtime: %.6f seconds\n", runtime);
     return 0;
 }
 
 
-int * sort(int len, int *left, int *array, int *sorted) {
+int * sort(int len, int *left, int *array, int arraylen, int *sorted) {
     if (len <= 1) {
         return left;
     }
@@ -50,8 +57,8 @@ int * sort(int len, int *left, int *array, int *sorted) {
     int offset = left - array;
     
     // sort left & right
-    left = sort(llen, left, array, sorted);
-    right = sort(rlen, right, array, sorted);
+    left = sort(llen, left, array, arraylen, sorted);
+    right = sort(rlen, right, array, arraylen, sorted);
 
     // merge
     int leftpoint = 0, rightpoint = 0;
@@ -71,10 +78,25 @@ int * sort(int len, int *left, int *array, int *sorted) {
         }
     }
 
+    printf("\r[ ");
+    for (int i = 0; i < arraylen; i++) {printf("%i ", array[i]);}
+    printf("]\n[ ");
+    for (int i = 0; i < arraylen; i++) {
+        if (sorted[i] != INT_MIN) {
+            printf("\x1b[1A░\x1b[1D\x1b[1B%i ", sorted[i]);
+        } else {
+            printf("░ ");
+        }
+    }
+    printf("]\x1b[1A");
+    fflush(stdout);
+    
     // write back into `array`
     for (int i = 0; i < len; i++) {
         array[i + offset] = sorted[i + offset];
+        sorted[i + offset] = INT_MIN;
     }
 
+    usleep(500000);
     return left;
 }
